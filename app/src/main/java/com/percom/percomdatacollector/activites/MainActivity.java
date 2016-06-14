8b415b5f8e47e7187c54fe673c01a0ec053254b6
t_rust01 <tristan.rust@gmx.de>
2016-06-14 13:01:19 +0200

    Fixed DELAY_FASTEST is now working.

--- app/src/main/java/com/percom/percomdatacollector/activites/MainActivity.java
+++ app/src/main/java/com/percom/percomdatacollector/activites/MainActivity.java
@@ -365,7 +365,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
                 // this.getFileService().deleteFile(FILE_NAME);
                 //  this.getFileService().saveAFile(arffFile, this.openFileOutput(arffFile.getStrFileName(), this.MODE_APPEND));
-                // testyll
                 this.getFileService().appendToFile(newRecord, this.openFileOutput(arffFile.getStrFileName(), this.MODE_APPEND));
 
             } else {
